9e40a3fb905dade9c1d4ec9af76b5200a3100575
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-07-31 22:47:21 -0700

    Create the Service Layer

--- src/main/java/com/devmountain/noteApp/entities/Note.java
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -9,9 +9,9 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Notes")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,29 +28,5 @@ public class Note {
         if (noteDto.getBody() != null) {
             this.body = noteDto.getBody();
         }
-    }
-
-    public Note(Long id, String body) {
-        this.id = id;
-        this.body = body;
-    }
-
-    public Note() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 }
--- src/main/java/com/devmountain/noteApp/entities/User.java
+++ src/main/java/com/devmountain/noteApp/entities/User.java
@@ -3,12 +3,18 @@ package com.devmountain.noteApp.entities;
 import com.devmountain.noteApp.dtos.UserDto;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,38 +37,5 @@ public class User {
         if (userDto.getPassword() != null) {
             this.password = userDto.getPassword();
         }
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
